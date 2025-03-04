127535e247ac38fd137a074849f5f2d2b7f8963f
bguerschom0 <bguerschom0@gmail.com>
2025-03-05 00:08:10 +0200

    Update UserManagement.jsx

--- src/pages/UserManagement/UserManagement.jsx
+++ src/pages/UserManagement/UserManagement.jsx
@@ -518,7 +518,7 @@ const UserManagement = () => {
                              bg-white dark:bg-gray-900 text-gray-900 dark:text-white
                              focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white"
                   />
-                  <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                  <Search className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
                 </div>
               </div>
               

4a885f68489236e56939f6ab86f104b88b2b3741
bguerschom0 <bguerschom0@gmail.com>
2025-03-07 01:21:10 +0200

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -359,7 +359,7 @@ const LoginPage = () => {
               required
               disabled={isLoading}
             />
-            <User className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-gray-500" />
+            <User className="absolute right-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-gray-500" />
           </div>
           
           <div className="relative">
@@ -375,7 +375,7 @@ const LoginPage = () => {
               required
               disabled={isLoading}
             />
-            <Lock className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-gray-500" />
+            <Lock className="absolute right-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-gray-500" />
           </div>
 
           <button 

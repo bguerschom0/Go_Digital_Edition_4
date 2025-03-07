de43b92b6828020d18c00628a5d2afac58ee7aa8
bguerschom0 <bguerschom0@gmail.com>
2025-03-07 16:51:00 +0200

    Update ModalOrganizationUsers.jsx

--- src/components/modals/ModalOrganizationUsers.jsx
+++ src/components/modals/ModalOrganizationUsers.jsx
@@ -354,7 +354,7 @@ const ModalOrganizationUsers = ({ isOpen, onClose, organizationId }) => {
         {/* Search bar */}
         <div className="mb-6">
           <div className="relative">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+            <Search className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
             <input
               type="text"
               placeholder="Search users by name, username, or email..."
